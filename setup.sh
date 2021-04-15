2b013045d285999f47a3b865cbff0b74f68327ae
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2021-04-15 11:14:21 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+cd $HOME/bomber
+mv bomb bomb.py /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod +x bomb bomb.py
+cd $HOME/bomber
+clear
+sleep 1.0
+echo -e "\033[1;92m   now type bomb]"
+rm setup.sh
