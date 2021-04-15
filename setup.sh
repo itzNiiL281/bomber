44a38ca2937aa0b8d139c95d6c984819a53c6a51
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2021-04-15 11:33:52 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+clear
+sleep 1.0
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pip install requests
+pip install pytube
+pip install termcolor
+pip install pyfiglet
+clear
+sleep 1.0
 cd $HOME/bomber
 mv bomb bomb.py /data/data/com.termux/files/usr/bin
 cd /data/data/com.termux/files/usr/bin
